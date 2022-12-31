251d58630f5bcce0836237635f7130d338d49216
Ayushrp <88778266+Ayushrp@users.noreply.github.com>
2023-01-01 00:36:55 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,9 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hi Friends Latest Devops Projects Uploaded Daily Based . LiKe & Share this video & also Subscribe";
+		<h1>Congratulations Luckymegha you have done it</h1>
+			<h2>As the night became darker with the stars that glows in sky make me realise how special you are and with this new beginning we gonna be at our best</h2>
+		return "At the end of the 2022 all our suffering got ended  and d we have completed your Devops project";
 	}
 
 	public static void main(String[] args) {
